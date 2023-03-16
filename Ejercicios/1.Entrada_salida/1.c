23b481aab0a2dfc5f6f2411971000575c03f84b6
eCanayUnsam <127110100+eCanayUnsam@users.noreply.github.com>
2023-03-16 17:35:59 -0300

    Update 1.c

--- Ejercicios/1.Entrada_salida/1.c
+++ Ejercicios/1.Entrada_salida/1.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
-void main() 
+int main() 
 	{
-		int a=20;
-		float b = 5.23478;
-		
-		printf("El valor de a es %d\n",a);
-		printf("El valor de b es %d\n",b);
+	int a=20;
+	float b = 5.23478;
+	
+	printf("El valor de a es %d\n",a);
+	printf("El valor de b es %d\n",b);
+	return 0;
 	}
