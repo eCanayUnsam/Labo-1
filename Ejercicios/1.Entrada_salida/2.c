9a309580cf31c5b952f3d01455697c6c3bfb63cd
eCanayUnsam <127110100+eCanayUnsam@users.noreply.github.com>
2023-03-15 21:08:38 -0300

    Create 2.c

---
+++ Ejercicios/1.Entrada_salida/2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void main()
+	{
+		int a=20;
+		float b = 5.23478;
+
+        int suma = a + b;
+
+		printf("El valor de a es %d\n",a);
+		printf("El valor de b es %f\n",b);
+		printf("El la suma de %d + %f es %f\n",a,b,suma);
+
+	}
