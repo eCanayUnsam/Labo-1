83afe3ff1f1af1eaaca9f12e5938feeebabd7957
eCanayUnsam <127110100+eCanayUnsam@users.noreply.github.com>
2023-03-15 21:17:12 -0300

    Create 3.c

---
+++ Ejercicios/1.Entrada_salida/3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main()
+	{
+		char a='h';
+		float b = 0.5;
+
+        int suma = b * a;
+
+		printf("El valor de a es %c\n",a);
+		printf("El valor de b es %f\n",b);
+		printf("El producto de %d * %f es %d\n",a,b,suma);
+	}
