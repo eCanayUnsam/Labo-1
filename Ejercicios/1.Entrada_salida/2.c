91726d987985aebc4f5ac07391e999e44ff17bc8
eCanayUnsam <127110100+eCanayUnsam@users.noreply.github.com>
2023-03-16 17:36:29 -0300

    Update 2.c

--- Ejercicios/1.Entrada_salida/2.c
+++ Ejercicios/1.Entrada_salida/2.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
-void main()
+int main()
 	{
-		int a=15;
-		float b = 0.5;
+	int a=15;
+	float b = 0.5;
 
         int suma = b * a;
 
-		printf("El valor de a es %d\n",a);
-		printf("El valor de b es %f\n",b);
-		printf("El producto de %d * %f es %d\n",a,b,suma);
-
+	printf("El valor de a es %d\n",a);
+	printf("El valor de b es %f\n",b);
+	printf("El producto de %d * %f es %d\n",a,b,suma);
+	
+	return 0;
 	}
